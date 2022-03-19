c91da8efc2acd0e737c6cd2479ad037b0d2940c7
Veronika Vashkevich <veronik.vashkevich@gmail.com>
2022-03-19 08:52:35 +0300

    product layout

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('title') Имя товара no-house @endsection
@@ -28,6 +27,55 @@
                                 38,54 BYN
                             </div>
                             <a href="#" class="btn btn-primary btn-cart">В корзину</a>
+                        </div>
+                    </div>
+                    <div class="product-body">
+                        <div class="product-description">
+                            Предназначена для окрашивания потолков в сухих помещениях. Краска ВД-АК-216 «DEKOR» для
+                            потолков, предназначена для окрашивания потолков в сухих помещениях.
+                            <br>
+                            Краску можно наносить на бетонные, кирпичные, деревянные, включая древесно-волокнистые,
+                            древесно-стружечные плиты, оштукатуренные, ранее окрашенные поверхности.
+                            <br>
+                            Легко наносится, быстро сохнет, не имеет резкого запаха. После высыхания образует ровную
+                            матовую поверхность белого цвета.
+                            <br>
+                            Пожаро- и взрывобезопасна, нетоксична.
+                        </div>
+                        <div class="product-parameters">
+                            <div class="block-title">
+                                <h5>Характеристики</h5>
+                            </div>
+                            <table class="parameters">
+                                <tr>
+                                    <td class="p-title">Производитель</td>
+                                    <td class="p-text">Россия</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Степень блеска</td>
+                                    <td class="p-text">Матовая</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Применение</td>
+                                    <td class="p-text">Потолки</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Время высыхания</td>
+                                    <td class="p-text">1 час</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Запах</td>
+                                    <td class="p-text">Отсутствует</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Цвет</td>
+                                    <td class="p-text">Белый</td>
+                                </tr>
+                                <tr>
+                                    <td class="p-title">Фасовка</td>
+                                    <td class="p-text">14 кг</td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
